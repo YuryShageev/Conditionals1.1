e129dcddc02c30d4a14d53f80061a0f424178465
YuryShageev <schageev.yur@yandex.ru>
2022-09-28 13:15:01 +0300

    Второй блок заданий

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Task 1");
+        int person = 17;
+        if (person >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        } else {
+            System.out.println("Потерпите ещё немного! Вам ещё нет восемнадцати!");
+        }
+
+        //Задание 2
+        System.out.println("Task 2");
+        int child = 9;
+        if (child >= 7) {
+            System.out.println("Ребёнок ходит в школу");
+        } else {
+            System.out.println("Ребёнку ещё рано идти в школу");
+        }
+        if (child >= 18) {
+            System.out.println("Ребёнок уже закончил школу");
+        } else {
+            System.out.println("Ребёнок ещё не закончил школу");
+        }
+        if (child >= 24) {
+            System.out.println("Уже взрослый");
+        } else {
+            System.out.println("Ещё не повзрослел");
+        }
     }
 }
